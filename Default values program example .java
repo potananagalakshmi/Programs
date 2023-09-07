c440d5aca67661f5c45178e831cc1e9705f73231
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-09-07 10:29:57 +0530

    Create Default values program example .java

---
+++ Default values program example .java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class VaraibleSizeArrays3 {
+    public static void main(String[] args) {
+    int[] arr=new int[10];
+    double[] arr1=new double[10];
+    String[] str=new String[10];
+    System.out.println(Arrays.toString(arr));
+    System.out.println(Arrays.toString(arr1));
+    System.out.println(Arrays.toString(str));
+    
+    
+}
+}
