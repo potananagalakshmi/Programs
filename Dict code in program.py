c305a0b05e10fd95c7b25d571a94d2508379f8d3
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-09-10 20:20:40 +0530

    Create Dict code in program.py

---
+++ Dict code in program.py
@@ -0,0 +1,7 @@
+data = { 'sachin': 100,
+       'kohli : 74'
+       'ponting':71,
+       'sanga':69}#As keys you can use only immutable types
+print(data.keys())
+print(data.values())
+print(data.items())
