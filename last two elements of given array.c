652f7fa37ca10be999bb294b468a866f4738c69e
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-09-13 23:15:33 +0530

    Create last two elements of given array.c

---
+++ last two elements of given array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+  char week[4][10] = {"Monday", "Tuesday", "Wednesday", "Thursday"};
+  printf("%s\n", week[ 2 ]);
+  printf("%s", week[ 3 ]);
+
+  return 0;
+}
