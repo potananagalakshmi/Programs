07f2ba7a136a78d2d892e4df053f8c2b5f52ca8b
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-09-14 23:24:07 +0530

    Create user input loop example.java

---
+++ user input loop example.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner read = new Scanner(System.in);
+		int num = read.nextInt();
+		int a = 0;
+		while(a < num){
+		    System.out.println(a);
+		    a++;
+		}
+	}
+}
