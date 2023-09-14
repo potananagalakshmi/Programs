0689c378e3d36f59608477ee9a1e517af561a764
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-09-14 23:24:53 +0530

    Create Do while loop  example .java

---
+++ Do while loop  example .java
@@ -0,0 +1,14 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		int i = 0;
+		do{
+		    System.out.println(i);
+		    i++;
+		}
+		while(i < 6);
+		
+	}
+}
