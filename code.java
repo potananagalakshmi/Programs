ce84d8b4072e91141befcd876faa41e64df3874f
Potana Naga Lakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2024-08-23 23:32:58 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		for(int i=0;i<a;i++)
+		{
+		    int b=sc.nextInt();
+		    int c=sc.nextInt();
+		    if(b<c)
+		    {
+		        System.out.println("yes");
+		    }
+		    else
+		    {
+		        System.out.println("NO");
+		    }
+		}
+
+	}
+}
